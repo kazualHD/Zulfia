b18e710ed428f63e9e2790eff1b646ea1dc33ebc
radmir <radmir@MSI>
2024-01-28 18:42:26 +0500

    fix

--- frontend/src/components/Parents.jsx
+++ frontend/src/components/Parents.jsx
@@ -1,11 +1,16 @@
 import pdf from "../assests/parents/pdf.png"
 import rod1 from "../assests/parents/rod1.png"
-import rod2 from "../assests/parents/rod2.png"
-import rod3 from "../assests/parents/rod3.png"
+import gamesI from "../assests/parents/games.png"
+import neyroI from "../assests/parents/neyro.png"
+import zan from "../assests/parents/zan.png"
+import golo from "../assests/parents/100.png"
 import '../styles/Parents.css';
+import cards from '../assests/parents/pdf/cards.pdf';
+import psysh from '../assests/parents/pdf/psyh.pdf';
+import neyro from '../assests/parents/pdf/neyro.pdf'
+import games from '../assests/parents/pdf/games.pdf'
 import gimnastka from '../assests/parents/pdf/gimnastika.pdf';
-import skorochet from '../assests/parents/pdf/skorochet.pdf';
-import mental from '../assests/parents/pdf/Igroteka.pdf';
+
 
 const Parents = () => {
     
@@ -25,18 +30,34 @@ const Parents = () => {
             </div>
             <hr width="70%"/>
             <div class="a2">
-                <div><img class="rod" src={rod2} alt=""/></div>
+                <div><img class="rod" src={gamesI} alt=""/></div>
                 <div class="a3">
                     <img class="skachat2" src={pdf} alt=""/>
-                    <div class="pdf" onClick={() => openPdf(mental)}>Открыть учебник</div>
+                    <div class="pdf" onClick={() => openPdf(games)}>Открыть учебник</div>
                 </div>
             </div>
             <hr width="70%"/>
             <div class="a2">
-                <div><img class="rod" src={rod3} alt=""/></div>
+                <div><img class="rod" src={neyroI} alt=""/></div>
                 <div class="a3">
                     <img class="skachat2" src={pdf} alt=""/>
-                    <div class="pdf" onClick={() => openPdf(skorochet)}>Открыть учебник</div>
+                    <div class="pdf" onClick={() => openPdf(neyro)}>Открыть учебник</div>
+                </div>
+            </div>
+            <hr width="70%"/>
+            <div class="a2">
+                <div><img class="rod" src={zan} alt=""/></div>
+                <div class="a3">
+                    <img class="skachat2" src={pdf} alt=""/>
+                    <div class="pdf" onClick={() => openPdf(psysh)}>Открыть учебник</div>
+                </div>
+            </div>
+            <hr width="70%"/>
+            <div class="a2">
+                <div><img class="rod" src={golo} alt=""/></div>
+                <div class="a3">
+                    <img class="skachat2" src={pdf} alt=""/>
+                    <div class="pdf" onClick={() => openPdf(cards)}>Открыть учебник</div>
                 </div>
             </div>
         </div>
--- frontend/src/components/Teachers.jsx
+++ frontend/src/components/Teachers.jsx
@@ -1,10 +1,8 @@
 import pdf from "../assests/teachers/pdf.png"
-import ped1 from "../assests/teachers/ped1.png"
-import ped2 from "../assests/teachers/ped2.png"
-import ped3 from "../assests/teachers/ped3.png"
-import book1 from '../assests/teachers/pdf/book1.pdf'
-import book2 from '../assests/teachers/pdf/book2.pdf'
-import book3 from '../assests/teachers/pdf/book3.pdf'
+import modelI from "../assests/teachers/model.png"
+import page37 from "../assests/teachers/page37.png"
+import modelP from "../assests/teachers/pdf/model.pdf"
+import page37P from "../assests/teachers/pdf/page37.pdf"
 import '../styles/Teachers.css';
 
 const Parents = () => {
@@ -14,18 +12,13 @@ const Parents = () => {
     return (
         <div class="t1">
         <div class="t2">
-            <div><img class="ucheb" src={ped1} alt=""/></div>
-            <div class="t3"><img class="skachat" src={pdf} alt=""/> <div className="pdf" onClick={()=> openPdf(book1)}>Открыть учебник</div></div>
+            <div><img class="ucheb" src={modelI} alt=""/></div>
+            <div class="t3"><img class="skachat" src={pdf} alt=""/> <div className="pdf" onClick={()=> openPdf(modelP)}>Открыть учебник</div></div>
         </div>
         <hr width="70%"/>
         <div class="t2">
-            <div><img class="ucheb" src={ped2} alt=""/></div>
-            <div class="t3"><img class="skachat" src={pdf} alt=""/><div className="pdf" onClick={()=> openPdf(book2)}>Открыть учебник</div></div>
-        </div>
-        <hr width="70%"/>
-        <div class="t2">
-            <div><img class="ucheb" src={ped3} alt=""/></div>
-            <div class="t3"><img class="skachat" src={pdf} alt=""/><div className="pdf" onClick={()=> openPdf(book3)}>Открыть учебник</div></div>
+            <div><img class="ucheb" src={page37} alt=""/></div>
+            <div class="t3"><img class="skachat" src={pdf} alt=""/><div className="pdf" onClick={()=> openPdf(page37P)}>Открыть учебник</div></div>
         </div>
       </div>
   )
